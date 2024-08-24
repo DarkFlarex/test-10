66ccb5cbbdd2e4ee5af3f5f18e8b09dcc6f97ad2
DarkFlarex <darkflarexxx@gmail.com>
2024-08-24 13:52:23 +0600

    Теперь во фронт добавили файл app в нем файлы hooks , store. Добавили файл axiosApi , constants

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {Provider} from "react-redux";
+import {BrowserRouter} from "react-router-dom";
+import {CssBaseline, ThemeProvider} from "@mui/material";
+import theme from "./theme.ts";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <Provider store={sotre}>
+        <BrowserRouter>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <App />
+            </ThemeProvider>
+        </BrowserRouter>
+    </Provider>
 )
